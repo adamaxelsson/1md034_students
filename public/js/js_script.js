f78f4319b45a0384093189ff117d4f8d61aff31e
adamaxelsson <adam.axelsson.4529@student.uu.se>
2020-02-04 12:09:12 +0100

    allergy fix

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,27 +1,55 @@
-function menuItem(name, info, allergies, kcal, img){
+function menuItem(name, info, kcal, img, lactose, vegan, gluten){
     this.name = name;
     this.info = info;
-    this.allergies = allergies;
     this.kcal = kcal;
     this.img = img;
+    this.lactose = lactose;
+    this.vegan = vegan;
+    this.gluten = gluten;
     this.getKcal = function(){
 	return this.name + ' ' + this.kcal;
     };
 }
 
-let burger1 = new menuItem("Bacon & Cheese Burger", "Beef from locally bred cattle", "Gluten free!", "800 kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg");
+let burger1 = new menuItem("Bacon & Cheese Burger", "Beef from locally bred cattle", "800 kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", false, false, true);
 
-let burger2 = new menuItem("Veggie Burger", "Patty made from beans!", "Vegan friendly", "600 kcal", "https://peasandcrayons.com/wp-content/uploads/2016/09/buffalo-chickpea-veggie-burgers-recipe-2-8313-300x300.jpg");
+let burger2 = new menuItem("Veggie Burger", "Patty made from beans!", "600 kcal", "https://peasandcrayons.com/wp-content/uploads/2016/09/buffalo-chickpea-veggie-burgers-recipe-2-8313-300x300.jpg", false, true, false);
 
-let burger3 = new menuItem("Chicken Burger", "Deep fried chicken", "Lactose free!", "700 kcal", "https://burmakadai.in/wp-content/uploads/2018/10/Chicken-Burger.jpg");
+let burger3 = new menuItem("Chicken Burger", "Deep fried chicken", "700 kcal", "https://burmakadai.in/wp-content/uploads/2018/10/Chicken-Burger.jpg", true, false, false);
 
-let burger4 = new menuItem("New Burger", "Info", "Allergies!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg");
+let burger4 = new menuItem("New Burger", "Info", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", true, false, false);
 
-let burger5 = new menuItem("New Burger", "Info", "Gluten free!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg");
+let burger5 = new menuItem("New Burger", "Info", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", false, false, true);
 
-document.getElementById("item1").innerHTML = burger1.getKcal();
-document.getElementById("item2").innerHTML = burger2.getKcal();
-document.getElementById("item3").innerHTML = burger3.getKcal();
-document.getElementById("item4").innerHTML = burger4.getKcal();
-document.getElementById("item5").innerHTML = burger5.getKcal();
+var array = new Array(burger1, burger2, burger3, burger4, burger5);
 
+let i = 0;
+let myElement = document.getElementById("burgerTable");
+for (i; i<array.length; i++){
+    let listItem = document.createElement("li");
+    let listValue = document.createTextNode(array[i].getKcal());
+    listItem.appendChild(listValue);
+    
+    if (array[i].lactose){
+	allergyList = document.createElement("li");
+	let allergy = document.createTextNode("Contains lactose!");
+	allergyList.appendChild(allergy);
+	listItem.appendChild(allergyList);
+    }
+    
+    if (array[i].vegan){
+	allergyList = document.createElement("li");
+	let allergy = document.createTextNode("Vegan friendly!");
+	allergyList.appendChild(allergy);
+	listItem.appendChild(allergyList);
+    }
+    
+    if (array[i].gluten){
+	allergyList = document.createElement("li");
+	let allergy = document.createTextNode("Contains gluten!");
+	allergyList.appendChild(allergy);
+	listItem.appendChild(allergyList);
+    }
+    
+    myElement.appendChild(listItem);
+}
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,32 +1,35 @@
-function menuItem(name, info, allergies, kcal, img){
+/**
+function menuItem(name, info, allergies, kcal, img, lactose, vegan, gluten){
     this.name = name;
     this.info = info;
     this.allergies = allergies;
     this.kcal = kcal;
     this.img = img;
+    this.lactose = lactose;
+    this.vegan = vegan;
+    this.gluten = gluten;
     this.getKcal = function(){
 	return this.name + ' ' + this.kcal;
     };
 }
 
-let burger1 = new menuItem("Bacon & Cheese Burger", "Beef from locally bred cattle", "Gluten free!", "800 kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg");
+let burger1 = new menuItem("Bacon & Cheese Burger", "Beef from locally bred cattle", "Gluten free!", "800 kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", true, false, false);
 
-let burger2 = new menuItem("Veggie Burger", "Patty made from beans!", "Vegan friendly", "600 kcal", "https://peasandcrayons.com/wp-content/uploads/2016/09/buffalo-chickpea-veggie-burgers-recipe-2-8313-300x300.jpg");
+let burger2 = new menuItem("Veggie Burger", "Patty made from beans!", "Vegan friendly", "600 kcal", "https://peasandcrayons.com/wp-content/uploads/2016/09/buffalo-chickpea-veggie-burgers-recipe-2-8313-300x300.jpg", false, true, false);
 
-let burger3 = new menuItem("Chicken Burger", "Deep fried chicken", "Lactose free!", "700 kcal", "https://burmakadai.in/wp-content/uploads/2018/10/Chicken-Burger.jpg");
+let burger3 = new menuItem("Chicken Burger", "Deep fried chicken", "Lactose free!", "700 kcal", "https://burmakadai.in/wp-content/uploads/2018/10/Chicken-Burger.jpg", false, false, true);
 
-let burger4 = new menuItem("New Burger", "Info", "Allergies!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg");
+let burger4 = new menuItem("New Burger", "Info", "Allergies!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", true, false, false);
 
-let burger5 = new menuItem("New Burger", "Info", "Gluten free!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg");
+let burger5 = new menuItem("New Burger", "Info", "Gluten free!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", false, false, true);
+
+var array = new Array(burger1, burger2, burger3, burger4, burger5);
 
 const vm = new Vue({
-  el: '#myID',
-  data: 
-  {
-    item1: burger1.getKcal(),
-    item2: burger2.getKcal(),
-    item3: burger3.getKcal(),
-    item4: burger4.getKcal(),
-    item5: burger5.getKcal(),
+	el: '#burgers',
+  data: {
+  array: array
   }
+
 })
+**/
