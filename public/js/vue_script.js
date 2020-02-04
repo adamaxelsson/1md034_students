fd23cdaffc35a2e0145ed57bfd143d6764dbb343
adamaxelsson <adam.axelsson.4529@student.uu.se>
2020-02-04 14:30:23 +0100

    vue

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -13,20 +13,20 @@ function menuItem(name, info, allergies, kcal, img, lactose, vegan, gluten){
     };
 }
 
-let burger1 = new menuItem("Bacon & Cheese Burger", "Beef from locally bred cattle", "Gluten free!", "800 kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", true, false, false);
+let burger1 = new menuItem("Cheese Burger", "Beef from locally bred cattle", "Gluten free!", "800 kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", true, false, false);
 
 let burger2 = new menuItem("Veggie Burger", "Patty made from beans!", "Vegan friendly", "600 kcal", "https://peasandcrayons.com/wp-content/uploads/2016/09/buffalo-chickpea-veggie-burgers-recipe-2-8313-300x300.jpg", false, true, false);
 
 let burger3 = new menuItem("Chicken Burger", "Deep fried chicken", "Lactose free!", "700 kcal", "https://burmakadai.in/wp-content/uploads/2018/10/Chicken-Burger.jpg", false, false, true);
 
-let burger4 = new menuItem("New Burger", "Info", "Allergies!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", true, false, false);
+let burger4 = new menuItem("Special Burger", "Info", "Allergies!", "900 kcal", "https://cdn-rdb.arla.com/Files/arla-se/1361781217/43b7c050-b61b-4baa-8eb6-5a8ebb896bc4.jpg?crop=(104,0,-152,0)&w=479&h=335&ak=f525e733&hm=72b785fb", true, false, false);
 
-let burger5 = new menuItem("New Burger", "Info", "Gluten free!", "kcal", "https://d1doqjmisr497k.cloudfront.net//-/media/franksus18/recipe-images/franks_redhot_bacon_cheeseburger_300x300.jpg", false, false, true);
+let burger5 = new menuItem("Burger Burger", "Info", "Gluten free!", " 750 kcal", "https://www.santamariaworld.com/optimized/maximum/globalassets/_recipes/bbq/kentucky_burger.jpg?id=2357", false, false, true);
 
 var array = new Array(burger1, burger2, burger3, burger4, burger5);
 
 const vm = new Vue({
-	el: '#burgers',
+	el: '#burgerTable',
   data: {
   array: array
   }
